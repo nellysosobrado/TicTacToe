ea08f0ac0ede5875d1eed2eaa37807398c290b11
nellysosobrado <Nelly.sosobrado@gmail.com>
2024-10-03 15:13:35 +0200

    New cool title!

--- TicTacToe_Game_GroupProject/Board.cs
+++ TicTacToe_Game_GroupProject/Board.cs
@@ -29,11 +29,12 @@ namespace TicTacToe_Game_GroupProject
 
         public void Display()
         {
-            Console.WriteLine($"{board[0]}|{board[1]}|{board[2]}");
+            
+            Console.WriteLine($"{board[6]}|{board[7]}|{board[8]}");
             Console.WriteLine("--+--+--");
             Console.WriteLine($"{board[3]}|{board[4]}|{board[5]}");
             Console.WriteLine("--+--+--");
-            Console.WriteLine($"{board[6]}|{board[7]}|{board[8]}");
+            Console.WriteLine($"{board[0]}|{board[1]}|{board[2]}");
         }
 
         public void ResetBoard()
