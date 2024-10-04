2188ca2c323221158596515710b159b3155a5d5e
Nelly <Nelly.sosobrado@gmail.com>
2024-10-04 17:02:58 +0200

    Improved gameboard design

--- TicTacToe_Game_GroupProject/Board.cs
+++ TicTacToe_Game_GroupProject/Board.cs
@@ -27,14 +27,67 @@ namespace TicTacToe_Game_GroupProject
             board[move - 1] = currentPlayerSymbol;
         }
 
-        public void Display()
+        public void Display(string currentPlayer)
         {
-            
-            Console.WriteLine($"{board[6]}|{board[7]}|{board[8]}");
-            Console.WriteLine("--+--+--");
-            Console.WriteLine($"{board[3]}|{board[4]}|{board[5]}");
-            Console.WriteLine("--+--+--");
-            Console.WriteLine($"{board[0]}|{board[1]}|{board[2]}");
+            Console.Clear(); // Rensa konsolen innan varje visning
+
+            // Ställ in Unicode-stöd för konsolen
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // Anpassa fönsterstorleken för att rymma brädan
+            Console.SetWindowSize(50, 20);
+
+            // Flytta ner texten för att lämna utrymme för att brädan hamnar i mitten
+            int emptyLinesBeforeBoard = (Console.WindowHeight - 15) / 2; // 15 rader för spelplan och raminformation
+            for (int i = 0; i < emptyLinesBeforeBoard; i++)
+            {
+                Console.WriteLine(); // Tomma rader för att centrera i höjdled
+            }
+
+            // Centrera rubriken för aktuell spelare och instruktionen
+            CenterText($"Player {currentPlayer}'s turn");
+            CenterText("Choose a number between 1-9");
+            Console.WriteLine(); // Extra rad för separation
+
+            // Rama in brädan och gör den större med raka linjer
+            CenterText("╔═════╦═════╦═════╗");
+            CenterText($"║  {FormatCell(board[6])}  ║  {FormatCell(board[7])}  ║  {FormatCell(board[8])}  ║");
+            CenterText("╠═════╬═════╬═════╣");
+            CenterText($"║  {FormatCell(board[3])}  ║  {FormatCell(board[4])}  ║  {FormatCell(board[5])}  ║");
+            CenterText("╠═════╬═════╬═════╣");
+            CenterText($"║  {FormatCell(board[0])}  ║  {FormatCell(board[1])}  ║  {FormatCell(board[2])}  ║");
+            CenterText("╚═════╩═════╩═════╝");
+            Console.WriteLine(); // Extra rad för separation
+        }
+
+        private string FormatCell(string cell)
+        {
+            // Ger färg beroende på om det är X eller O
+            if (cell == "X")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else if (cell == "O")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White; // Standardfärg för tomma celler
+            }
+
+            string formattedCell = $"{cell}"; // Behåll cellens värde
+            Console.ResetColor(); // Återställ färgen för att inte påverka resten av texten
+            return formattedCell;
+        }
+
+        private void CenterText(string text)
+        {
+            // Hämta konsolens bredd och räkna ut var texten ska börja
+            int windowWidth = Console.WindowWidth;
+            int centeredPosition = (windowWidth - text.Length) / 2;
+            Console.SetCursorPosition(centeredPosition, Console.CursorTop); // Sätt textens position
+            Console.WriteLine(text);
         }
 
         public void ResetBoard()
--- TicTacToe_Game_GroupProject/Game.cs
+++ TicTacToe_Game_GroupProject/Game.cs
@@ -9,81 +9,162 @@ namespace TicTacToe_Game_GroupProject
 {
     public class Game
     {
-        private PlayerManager players;
-        private Board board;
-        private WinDrawLossChecker winnerChecker;
+        private string[] board = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-        public Game()
-        {
-            players = new PlayerManager();
-            board = new Board();
-            winnerChecker = new WinDrawLossChecker();
-        }
+        public string[] BoardState => board;
 
         public void Start()
         {
-            players.PlayerDetails();
-            bool gameOn = true;
-            int gameCounter = 0;
+            string currentPlayer = "1";
+            bool isGameRunning = true;
 
-            while (gameOn)
+            // Spelets huvudloop
+            while (isGameRunning)
             {
-                Console.Clear();
-                board.Display();
-                Player currentPlayer = players.GetCurrentPlayer();
-                Console.WriteLine($"Player {currentPlayer.Name}'s turn ({currentPlayer.Symbol})");
-                Console.WriteLine("Choose a number between 1-9");
+                // Visa brädan och aktuell spelares tur
+                Display(currentPlayer);
 
+                // Be spelaren välja en ruta
+                CenterText($"Player {currentPlayer}, choose a number between 1-9: ");
                 string userInput = Console.ReadLine();
 
-                if (board.IsValidMove(userInput))
+                // Kontrollera om draget är giltigt
+                if (IsValidMove(userInput))
                 {
-                    board.MakeMove(userInput, currentPlayer.Symbol);
-                    gameCounter++;
+                    // Utför draget och byta spelare
+                    MakeMove(userInput, currentPlayer == "1" ? "X" : "O");
 
-                    if (winnerChecker.CheckWinner(board.BoardState, currentPlayer.Symbol))
+                    // Kolla om någon vinner eller om det blir oavgjort
+                    if (CheckForWinner())
                     {
-                        board.Display();
-                        Console.WriteLine($"\nPlayer {currentPlayer.Name} wins!");
-                        gameOn = false;
-                        AskToPlayAgain(ref gameOn, ref gameCounter);
+                        Display(currentPlayer); // Visa brädan en sista gång
+                        Console.WriteLine($"Player {currentPlayer} wins!");
+                        isGameRunning = false;
                     }
-                    else if (gameCounter == 9)
+                    else if (IsBoardFull())
                     {
-                        board.Display();
-                        Console.WriteLine("\nIt's a draw!");
-                        gameOn = false;
-                        AskToPlayAgain(ref gameOn, ref gameCounter);
+                        Display(currentPlayer); // Visa brädan en sista gång
+                        Console.WriteLine("It's a draw!");
+                        isGameRunning = false;
                     }
-                    else
-                    {
-                        players.PlayerOrder(); // Växla spelare
-                    }
+
+                    // Växla spelare
+                    currentPlayer = (currentPlayer == "1") ? "2" : "1";
                 }
                 else
                 {
-                    Console.WriteLine($"'{userInput}' is not a valid input." +
-                        "\nPress any key to continue...");
-                    Console.ReadKey();
+                    Console.WriteLine("Invalid move! Please try again.");
+                    Console.ReadKey(); // Vänta på att användaren trycker på en knapp
                 }
             }
         }
 
-        // Frågar om spelarna vill spela igen
-        private void AskToPlayAgain(ref bool gameOn, ref int gameCounter)
+        public bool IsValidMove(string userInput)
         {
-            Console.WriteLine("Do you want to play again? (y/n)");
-            string playAgain = Console.ReadLine().ToLower();
-            if (playAgain == "y")
+            if (int.TryParse(userInput, out int move) && move >= 1 && move <= 9)
             {
-                board.ResetBoard();
-                gameCounter = 0;
-                gameOn = true;
+                return board[move - 1] != "X" && board[move - 1] != "O";
+            }
+            return false;
+        }
+
+        public void MakeMove(string userInput, string currentPlayerSymbol)
+        {
+            int move = int.Parse(userInput);
+            board[move - 1] = currentPlayerSymbol;
+        }
+
+        public void Display(string currentPlayer)
+        {
+            Console.Clear(); // Rensa konsolen innan varje visning
+
+            // Flytta ner texten för att lämna utrymme för att brädan hamnar i mitten
+            int emptyLinesBeforeBoard = (Console.WindowHeight - 15) / 2; // 15 rader för spelplan och raminformation
+            for (int i = 0; i < emptyLinesBeforeBoard; i++)
+            {
+                Console.WriteLine(); // Tomma rader för att centrera i höjdled
+            }
+
+            // Centrera rubriken för aktuell spelare och instruktionen
+            CenterText($"Player {currentPlayer}'s turn");
+            CenterText("Choose a number between 1-9");
+            Console.WriteLine(); // Extra rad för separation
+
+            // Rama in brädan och gör den större med raka linjer
+            CenterText("╔═════╦═════╦═════╗");
+            CenterText($"║  {FormatCell(board[6])}  ║  {FormatCell(board[7])}  ║  {FormatCell(board[8])}  ║");
+            CenterText("╠═════╬═════╬═════╣");
+            CenterText($"║  {FormatCell(board[3])}  ║  {FormatCell(board[4])}  ║  {FormatCell(board[5])}  ║");
+            CenterText("╠═════╬═════╬═════╣");
+            CenterText($"║  {FormatCell(board[0])}  ║  {FormatCell(board[1])}  ║  {FormatCell(board[2])}  ║");
+            CenterText("╚═════╩═════╩═════╝");
+            Console.WriteLine(); // Extra rad för separation
+        }
+
+        private string FormatCell(string cell)
+        {
+            // Ger färg beroende på om det är X eller O
+            if (cell == "X")
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else if (cell == "O")
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
             }
             else
             {
-                Console.WriteLine("Thank you for playing!\nProgram is closing...");
+                Console.ForegroundColor = ConsoleColor.White; // Standardfärg för tomma celler
             }
+
+            string formattedCell = $"{cell}"; // Behåll cellens värde
+            Console.ResetColor(); // Återställ färgen för att inte påverka resten av texten
+            return formattedCell;
+        }
+
+        private void CenterText(string text)
+        {
+            // Hämta konsolens bredd och räkna ut var texten ska börja
+            int windowWidth = Console.WindowWidth;
+            int centeredPosition = (windowWidth - text.Length) / 2;
+            Console.SetCursorPosition(centeredPosition, Console.CursorTop); // Sätt textens position
+            Console.WriteLine(text);
+        }
+
+        public void ResetBoard()
+        {
+            board = new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+        }
+
+        // Kolla om det finns en vinnare
+        private bool CheckForWinner()
+        {
+            string[,] winningCombinations = {
+                { board[0], board[1], board[2] },
+                { board[3], board[4], board[5] },
+                { board[6], board[7], board[8] },
+                { board[0], board[3], board[6] },
+                { board[1], board[4], board[7] },
+                { board[2], board[5], board[8] },
+                { board[0], board[4], board[8] },
+                { board[2], board[4], board[6] }
+            };
+
+            foreach (var combination in winningCombinations)
+            {
+                if (combination[0] == combination[1] && combination[1] == combination[2])
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        // Kolla om brädan är full
+        private bool IsBoardFull()
+        {
+            return board.All(s => s == "X" || s == "O");
         }
     }
 }
