5c340627729ad0b291625b872c8df63118ab3941
Nelly <Nelly.sosobrado@gmail.com>
2024-10-06 12:18:02 +0200

    Game.cs uppdate

--- TicTacToe_Game_GroupProject/Game.cs
+++ TicTacToe_Game_GroupProject/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Numerics;
 using System.Text;
@@ -7,66 +8,77 @@ using System.Threading.Tasks;
 
 namespace TicTacToe_Game_GroupProject
 {
+    
     public class Game
     {
         private Board board = new Board(); // Använder din Board-klass
 
-        // Lägg till denna Start-metod om den saknas
+        // Startar spelet
         public void Start()
         {
             string currentPlayer = "1"; // Spelare 1 startar
             bool isGameRunning = true;
-            string errorMessage = ""; // Hanterar felmeddelanden
 
             // Spelets huvudloop
             while (isGameRunning)
             {
-                // Visa brädan och låt spelaren navigera och göra sitt drag
-                bool validMove = board.NavigateAndMakeMove(currentPlayer == "1" ? "X" : "O", out errorMessage);
+                string marker = GetMarker(currentPlayer); // Hämta markör baserat på spelare
+                bool validMove = board.NavigateAndMakeMove(marker, out string errorMessage);
 
                 if (!validMove)
                 {
-
                     board.Display(currentPlayer, currentPlayer == "1" ? "X" : "O", errorMessage);
                     Console.ReadKey(); // Vänta på att användaren trycker på en knapp för att se meddelandet
                     currentPlayer = (currentPlayer == "1") ? "2" : "1"; // Växla spelare
                     continue; // Hoppa över resten av loopen för att gå direkt till nästa spelare
                 }
 
+                board.Display(currentPlayer, marker); // Visa brädan
+
                 // Kolla om någon har vunnit eller om det blivit oavgjort
                 if (CheckForWinner())
                 {
-                    board.Display(currentPlayer, currentPlayer == "1" ? "X" : "O"); // Visa brädan en sista gång
                     Console.WriteLine($"Player {currentPlayer} wins!");
                     isGameRunning = false;
                 }
                 else if (IsBoardFull())
                 {
-                    board.Display(currentPlayer, currentPlayer == "1" ? "X" : "O"); // Visa brädan en sista gång
                     Console.WriteLine("It's a draw!");
                     isGameRunning = false;
                 }
-
-                // Växla spelare efter ett giltigt drag
-                currentPlayer = (currentPlayer == "1") ? "2" : "1";
-                errorMessage = ""; // Nollställ felmeddelandet för nästa runda
+                else
+                {
+                    // Växla spelare efter ett giltigt drag
+                    currentPlayer = SwitchPlayer(currentPlayer);
+                }
             }
         }
 
-        // Övriga metoder som CheckForWinner och IsBoardFull ska också vara med här
+        // Växlar till nästa spelare
+        private string SwitchPlayer(string currentPlayer)
+        {
+            return currentPlayer == "1" ? "2" : "1";
+        }
+
+        // Hämtar markör baserat på spelare
+        private string GetMarker(string currentPlayer)
+        {
+            return currentPlayer == "1" ? "X" : "O";
+        }
+
         // Kolla om någon har vunnit
         private bool CheckForWinner()
         {
             int[,] winningCombinations = {
-            { 0, 1, 2 },
-            { 3, 4, 5 },
-            { 6, 7, 8 },
-            { 0, 3, 6 },
-            { 1, 4, 7 },
-            { 2, 5, 8 },
-            { 0, 4, 8 },
-            { 2, 4, 6 }
-        };
+                { 0, 1, 2 },
+                { 3, 4, 5 },
+                { 6, 7, 8 },
+                { 0, 3, 6 },
+                { 1, 4, 7 },
+                { 2, 5, 8 },
+                { 0, 4, 8 },
+                { 2, 4, 6 }
+            };
 
             for (int i = 0; i < winningCombinations.GetLength(0); i++)
             {
@@ -90,6 +102,6 @@ namespace TicTacToe_Game_GroupProject
         {
             return board.BoardState.All(s => s == "X" || s == "O");
         }
-        
+
     }
 }
